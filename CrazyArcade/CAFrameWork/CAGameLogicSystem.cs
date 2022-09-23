04c8337f981dac5d9077fc2af631740872f209e4
Clifftop7 <78198754+Clifftop7@users.noreply.github.com>
2022-09-23 12:41:47 -0400

    Added game system for handling object specific logic for ordinary ISprite objects

---
+++ CrazyArcade/CAFrameWork/CAGameLogicSystem.cs
@@ -0,0 +1,42 @@
+﻿using CrazyArcade.CAFramework.Controller;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyArcade.CAFramework
+{
+    public class CAGameLogicSystem : IGameSystem
+    {
+        List<ISprite> sprites = new List<ISprite>();
+        public CAGameLogicSystem()
+        {
+            sprites = new List<ISprite>();
+        }
+
+        public void AddSprite(ISprite sprite)
+        {
+            sprites.Add(sprite);
+        }
+
+        public void RemoveAll()
+        {
+            sprites = new List<ISprite>();
+        }
+
+        public bool RemoveSprite(ISprite sprite)
+        {
+            return sprites.Remove(sprite);
+        }
+
+        public void Update(GameTime time)
+        {
+            foreach (ISprite sprite in sprites)
+            {
+                sprite.Update(time);
+            }
+        }
+    }
+}
