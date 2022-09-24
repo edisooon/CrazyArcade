219dc017410eff1079cfb2007bde3d2455672e7d
Edison Zhang <ezrabbit@ezrabbits-MBP.lan>
2022-09-24 17:03:15 -0400

    added IEntity

---
+++ CrazyArcade/CAFrameWork/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrazyArcade.CAFrameWork
+{
+    public interface IEntity
+    {
+        void Load();
+        void Update(GameTime time);
+        void Draw(GameTime time, SpriteBatch batch);
+    }
+}
+
