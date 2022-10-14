8b73884ac9bf78eb5703a2951aef10afeab027ae
Clifftop7 <78198754+Clifftop7@users.noreply.github.com>
2022-10-14 11:52:59 -0400

    Begin work on the new branch

---
+++ CrazyArcade/Blocks/IBlockCollision.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyArcade.Blocks
+{
+    internal interface IBlockCollision
+    {
+        public Rectangle boundingBox {get; set;}
+
+        public void CollisionLogic();
+    }
+}
---
+++ CrazyArcade/CAFrameWork/CollisionSystem/BlockCollisionSystem.cs
@@ -0,0 +1,35 @@
+﻿using CrazyArcade.CAFramework;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyArcade.CAFrameWork.CollisionSystem
+{
+    internal class BlockCollisionSystem : IGameSystem
+    {
+        List<IEntity> blockList = new List<IEntity>();
+        List<IEntity> blockColidableList = new List<IEntity>();
+        public void AddSprite(IEntity sprite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSprite(IEntity sprite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
