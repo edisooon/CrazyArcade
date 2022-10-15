ef8cf421afd1f64a54abe023e08e68a69a8a8a6b
AW186 <57668160+AW186@users.noreply.github.com>
2022-10-15 11:09:56 -0400

    completed grid systems with sun boss as demo

---
+++ CrazyArcade/GridSystems/IGridable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace CrazyArcade.GameGridSystems
+{
+	public interface IGridable
+	{
+		Vector2 ScreenCoord { get; set; }
+        Vector2 GameCoord { get; set; }
+    }
+}
+
