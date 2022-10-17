e054eaf97082093ccb6436238996ba73921e2b44
jacobstrasburg <113728601+jacobstrasburg@users.noreply.github.com>
2022-10-16 20:31:37 -0400

    Starting item collision

---
+++ CrazyArcade/Items/IItemCollision.cs
@@ -0,0 +1,26 @@
+﻿using CrazyArcade.PlayerStateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyArcade.Items
+{
+    public interface IItemCollision
+    {
+    }
+    public class PowerupCollision
+    {
+        private PlayerCharacter player;
+
+        public PowerupCollision(PlayerCharacter player)
+        {
+            this.player = player;
+        }
+        public void CollisionCheck()
+        {
+
+        }
+    }
+}
