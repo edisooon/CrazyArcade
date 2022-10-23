ae8bcae83c299083d652bd1dfb3321d2c1be5af2
Clifftop7 <78198754+Clifftop7@users.noreply.github.com>
2022-10-23 13:41:35 -0400

    First changes, deleting commented out code and writing some comments

--- CrazyArcade/Blocks/IBlockCollision.cs
+++ CrazyArcade/Blocks/IBlockCollision.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 
 namespace CrazyArcade.Blocks
 {
+    //The interface for handling block collision. This will be implemented by blocks that will serve as a barrier to entnties that will collide with it.
     public interface IBlockCollision
     {
+        //The rectangle that is collided with
         public Rectangle boundingBox {get;}
+        //The code that is executed when a collision is detected
         public void CollisionLogic(Rectangle overlap, IBlockCollidable collisionPartner);
     }
 }
