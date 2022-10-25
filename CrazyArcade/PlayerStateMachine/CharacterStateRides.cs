54708458f22822d4f02b24e32e2bf5fa849692ab
AW186 <57668160+AW186@users.noreply.github.com>
2022-10-24 22:48:53 -0400

    save

--- CrazyArcade/PlayerStateMachine/CharacterStateRides.cs
+++ CrazyArcade/PlayerStateMachine/CharacterStateRides.cs
@@ -1,26 +1,33 @@
 ﻿using System;
+using System.Net.Http;
+using CrazyArcade.BombFeature;
 using CrazyArcade.CAFramework;
 using CrazyArcade.Content;
 using Microsoft.Xna.Framework;
 using CrazyArcade.Items;
+using Microsoft.Xna.Framework.Input;
 
 namespace CrazyArcade.PlayerStateMachine
 {
+    //overall goal is to keep ride synced with player, speed, location, and direction.
+    //This class should only be able to lead to the free state, implementing the extra life
     public class CharacterStateRides : ICharacterState
     {
-        int turtleSpeed = 2;
-        public SpriteAnimation[] spriteAnims;
+        Ride mount;
         private Character character;
+        private bool d1HeldDown;
+        private bool d2HeldDown;
 
-        public CharacterStateRides(Character character)
+        public CharacterStateRides(Character character, int ride)
         {
-            spriteAnims = new SpriteAnimation[4];
-            spriteAnims[0] = new SpriteAnimation(TextureSingleton.GetRides(), 12, 4, 4, 96, 32, 3);
-            spriteAnims[1] = new SpriteAnimation(TextureSingleton.GetRides(), 12, 4, 37, 96, 32, 3);
-            spriteAnims[2] = new SpriteAnimation(TextureSingleton.GetRides(), 12, 4, 68, 96, 32, 3);
-            spriteAnims[3] = new SpriteAnimation(TextureSingleton.GetRides(), 12, 4, 100, 96, 32, 3);
-
+            //int ride used to indicate turtle, pirate turtle, UFO, or owl [0-3]
+            //only use 0 for now
             this.character = character;
+            Ride newRide = new(character, ride);
+            mount = newRide;
+            d1HeldDown = false;
+            d2HeldDown = false;
+            character.parentScene.AddSprite(mount);
         }
 
         public void ProcessAttaction()
@@ -28,8 +35,9 @@ namespace CrazyArcade.PlayerStateMachine
             // there shall be a way to store the free state inside player character
             // rather than create a new one (preserve the powerups)
 
-            //pesudo code:
-            // character.playerState = character.playerFree;
+            //character.playerState = character.PlayerStateFree;
+            //add ride here?
+
         }
 
         public void ProcessItem()
@@ -42,21 +50,55 @@ namespace CrazyArcade.PlayerStateMachine
         {
             // shall do nothing
         }
-
         public void ProcessState(GameTime time)
         {
-            throw new NotImplementedException();
+            //duplicate code, fix
+            character.CalculateMovement();
+            character.UpdatePosition();
+            character.animationHandleInt = (int)character.direction;
+            character.playerState = new CharacterStateRides(character, 0);
+            character.spriteAnims = character.playerState.SetSprites();
+            character.playerState.SetSpeed();
+            if (character.CurrentSpeed.X == 0 && character.CurrentSpeed.Y == 0)
+            {
+                character.SpriteAnim.playing = false;
+                character.SpriteAnim.setFrame(0);
+            }
+            else
+            {
+                character.SpriteAnim.playing = true;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.E))
+            {
+                character.playerState = new CharacterStateBubble(character);
+                character.spriteAnims = character.playerState.SetSprites();
+                character.playerState.SetSpeed();
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D1) && !d1HeldDown)
+            {
+                d1HeldDown = true;
+                character.currentBlastLength = character.currentBlastLength + 1 < 5 ? character.currentBlastLength + 1 : 5;
+            }
+            d1HeldDown = Keyboard.GetState().IsKeyDown(Keys.D1);
+            if (Keyboard.GetState().IsKeyDown(Keys.D2) && !d2HeldDown)
+            {
+                d2HeldDown = true;
+                character.bombCapacity = character.bombCapacity + 1 < 3 ? character.bombCapacity + 1 : 3;
+            }
+            d2HeldDown = Keyboard.GetState().IsKeyDown(Keys.D2);
         }
 
         public int SetSpeed()
+
         {
-            return turtleSpeed;
+            character.ModifiedSpeed = 7;
+            return 1;
         }
 
         public SpriteAnimation[] SetSprites()
         {
-            return spriteAnims;
+            SpriteAnimation[] newSprites = mount.spriteAnims;
+            return newSprites;
         }
     }
 }
-
