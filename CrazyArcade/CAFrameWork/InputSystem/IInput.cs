722e5f8f901c623d6cc0676183e4d5635b9383fa
AW186 <57668160+AW186@users.noreply.github.com>
2022-11-03 15:57:53 -0400

    save

---
+++ CrazyArcade/CAFrameWork/InputSystem/IInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CrazyArcade.CAFramework;
+
+namespace CrazyArcade.CAFrameWork.InputSystem
+{
+	public interface IInput: IEntity
+	{
+		HashSet<int> GetInputs();
+	}
+}
+
---
+++ CrazyArcade/CAFrameWork/InputSystem/InputSystems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CrazyArcade.CAFramework;
+using Microsoft.Xna.Framework;
+
+namespace CrazyArcade.CAFrameWork.InputSystem
+{
+	public class InputSystems: IGameSystem
+	{
+		public InputSystems()
+		{
+		}
+
+        public void AddSprite(IEntity sprite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveSprite(IEntity sprite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
