ee76680ca2a794556db93b9c79daa15e30c9aaf3
AW186 <57668160+AW186@users.noreply.github.com>
2022-11-07 13:48:25 -0500

    Input System first version

--- CrazyArcade/CAFrameWork/InputSystem/InputSystems.cs
+++ CrazyArcade/CAFrameWork/InputSystem/InputSystems.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CrazyArcade.CAFramework;
 using Microsoft.Xna.Framework;
 
@@ -6,28 +7,58 @@ namespace CrazyArcade.CAFrameWork.InputSystem
 {
 	public class InputSystems: IGameSystem
 	{
+        List<IInput> inputs;
+        List<IInputController> controllers;
+        HashSet<int> inputKeys;
 		public InputSystems()
 		{
 		}
 
         public void AddSprite(IEntity sprite)
         {
-            throw new NotImplementedException();
+            if (sprite is IInput)
+            {
+                inputs.Add(sprite as IInput);
+            }
+            if (sprite is IInputController)
+            {
+                controllers.Add(sprite as IInputController);
+            }
         }
 
         public void RemoveAll()
         {
-            throw new NotImplementedException();
+            inputs = new List<IInput>();
+            controllers = new List<IInputController>();
         }
 
         public void RemoveSprite(IEntity sprite)
         {
-            throw new NotImplementedException();
+            if (sprite is IInput)
+            {
+                inputs.Remove(sprite as IInput);
+            }
+            if (sprite is IInputController)
+            {
+                controllers.Remove(sprite as IInputController);
+            }
         }
 
         public void Update(GameTime time)
         {
-            throw new NotImplementedException();
+            inputKeys = new HashSet<int>();
+            foreach(IInput input in inputs)
+            {
+                inputKeys.UnionWith(input.GetInputs());
+            }
+            foreach(IInputController controller in controllers)
+            {
+                Dictionary<int, Action> commands = controller.getCommands();
+                foreach(int key in inputKeys)
+                {
+                    commands[key]();
+                }
+            }
         }
     }
 }
