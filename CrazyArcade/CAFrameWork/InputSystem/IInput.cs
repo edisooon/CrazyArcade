1eb20bf1ea67b442ba444d89ceffd2cdf9503026
AW186 <57668160+AW186@users.noreply.github.com>
2022-11-07 17:38:44 -0500

    Comments on IInput

--- CrazyArcade/CAFrameWork/InputSystem/IInput.cs
+++ CrazyArcade/CAFrameWork/InputSystem/IInput.cs
@@ -4,8 +4,12 @@ using CrazyArcade.CAFramework;
 
 namespace CrazyArcade.CAFrameWork.InputSystem
 {
+
 	public interface IInput: IEntity
 	{
+		/* Return all the keys that in active for a input
+		 * e.g. w key down is 1, w key clicked is 2, s key down is 3, ...
+		 */
 		HashSet<int> GetInputs();
 	}
 }
