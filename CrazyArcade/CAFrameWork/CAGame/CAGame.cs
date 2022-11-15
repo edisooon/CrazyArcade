f63a841bd1d0e3175fbaf28614fb1a5505eaf582
Ardon Wu <ardonmxwu@gmail.com>
2022-11-14 20:53:18 -0500

    levels 1 and 2 removed, oct test level removed

--- CrazyArcade/CAFrameWork/CAGame/CAGame.cs
+++ CrazyArcade/CAFrameWork/CAGame/CAGame.cs
@@ -84,7 +84,7 @@ public class CAGame : Game, IGameDelegate, ITransitionCompleteHandler
         //Load it here
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        LevelSongTitles = new string[] { "playground", "playground", "playground", "comical", "bridge", "dream", "kodama", "worldbeat", "funtimes", "funtimes", "funtimes", "comical" };
+        LevelSongTitles = new string[] { "playground", "comical", "bridge", "dream", "kodama", "worldbeat", "funtimes", "funtimes", "comical" };
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         test = new ReadJSON("Level_0.json", ReadJSON.fileType.LevelFile);
         CurrentLevel = test.levelObject;
